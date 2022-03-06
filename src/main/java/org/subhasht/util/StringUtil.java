8424f976b19660cc0877bddb5e79a6f731f4584a
subhash <subhash@todkari.com>
2022-03-06 12:56:14 -0500

    String util class

---
+++ src/main/java/org/subhasht/util/StringUtil.java
@@ -0,0 +1,19 @@
+package org.subhasht.util;
+
+public class StringUtil {
+
+    public static void main(String[] args) {
+        System.out.println(skipSpecialChars("Most Common Word") + "_" + 819);
+    }
+
+    public static String skipSpecialChars(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str.length());
+        for(char c : str.toCharArray()) {
+            if( (c >= 'a' && c <= 'z')
+                    || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
