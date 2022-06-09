b94a1bb8d8757cc2bac2131754384b0a3499ce88
subhash <subhash@todkari.com>
2022-06-09 02:01:40 -0400

    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- src/main/java/org/subhasht/practice/leetcode/problems/LongestSubstringWithoutRepeatingCharacters_3.java
+++ src/main/java/org/subhasht/practice/leetcode/problems/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -42,69 +42,73 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
         Assertions.assertEquals(1, solution.lengthOfLongestSubstring("aaaaaa"));
     }
 
-}
 
-//fastest - sliding window
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int [] indexMap = new int [256];
-        Arrays.fill(indexMap, -1);
+    //fastest - sliding window
+    static class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int [] indexMap = new int [256];
+            Arrays.fill(indexMap, -1);
 
-        char [] arr = s.toCharArray();
-        int start = 0, max = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if( indexMap[ arr[i] ] != -1 ) {
-                while( start <= indexMap[ arr[i] ] ) {
-                    indexMap[ arr[start++] ] = -1;
+            char [] arr = s.toCharArray();
+            int start = 0, max = 0;
+            for(int i = 0; i < arr.length; i++) {
+                if( indexMap[ arr[i] ] != -1 ) {
+                    while( start <= indexMap[ arr[i] ] ) {
+                        indexMap[ arr[start++] ] = -1;
+                    }
+                } else {
+                    if( i - start + 1 > max)
+                        max = i - start + 1;
                 }
-            } else {
-                if( i - start + 1 > max)
-                    max = i - start + 1;
+                indexMap[ arr[i] ] = i;
             }
-            indexMap[ arr[i] ] = i;
+
+            return max;
         }
-
-        return max;
     }
-}
 
-//fastest - sliding window
-class Solution2 {
-    public int lengthOfLongestSubstring(String s) {
-        int start = 0, l, max = 0;
-        int [] positions = new int [127];
-        Arrays.fill(positions, -1);
-        //System.out.println(Arrays.toString(positions));
-        for(int i = 0; i < s.length(); i++) {
-            int c = s.charAt(i);
-            if( positions[c] != -1 && positions[c] >= start) {
-                l = i - start;
-                max = Math.max(max, l);
-                start = positions[c] + 1;
-            }
-            positions[c] = i;
+    //fastest - sliding window
+    static class Solution2 {
+        public int lengthOfLongestSubstring(String s) {
+            int start = 0, l, max = 0;
+            int [] positions = new int [127];
+            Arrays.fill(positions, -1);
             //System.out.println(Arrays.toString(positions));
+            for(int i = 0; i < s.length(); i++) {
+                int c = s.charAt(i);
+                if( positions[c] != -1 && positions[c] >= start) {
+                    l = i - start;
+                    max = Math.max(max, l);
+                    start = positions[c] + 1;
+                }
+                positions[c] = i;
+                //System.out.println(Arrays.toString(positions));
+            }
+            return Math.max(max, s.length() - start);
         }
-        return Math.max(max, s.length() - start);
     }
+
+    //slower - linear search
+    static class Solution1 {
+        public int lengthOfLongestSubstring(String s) {
+            int start = 0;
+            int max = 0, l = 0;
+            for(int i = 0; i < s.length(); i++) {
+                int idx = s.indexOf(s.charAt(i), start);
+                if (idx >= start && idx < i) {
+                    max = Math.max(max, l);
+                    start = idx + 1;
+                    l = i - start + 1;
+                } else {
+                    l++;
+                }
+                System.out.println(i + "] start: " + start + ", l : " + l + ", max: " + max);
+            }
+            return Math.max(max, l);
+        }
+    }
+
 }
 
-//slower - linear search
-class Solution1 {
-    public int lengthOfLongestSubstring(String s) {
-        int start = 0;
-        int max = 0, l = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int idx = s.indexOf(s.charAt(i), start);
-            if (idx >= start && idx < i) {
-                max = Math.max(max, l);
-                start = idx + 1;
-                l = i - start + 1;
-            } else {
-                l++;
-            }
-            System.out.println(i + "] start: " + start + ", l : " + l + ", max: " + max);
-        }
-        return Math.max(max, l);
-    }
-}
+
+
